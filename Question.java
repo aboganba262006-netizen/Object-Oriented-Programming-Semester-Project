0a3cbe8fc7198c9d85bd8c93f9ab48cedf2beef8
yahia maher khalil <yahiamaherkhalil@yahias-MacBook-Air.local>
2025-11-29 16:03:01 +0200

    added class question

---
+++ Question.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Question {
+    String text;
+    String[] options;
+    int correct;
+
+    public Question(String text, String[] options, int correct) {
+        this.text = text;
+        this.options = options;
+        this.correct = correct;
+    }
+
+    public void show() {
+        System.out.println(text);
+        for (int i = 0; i < options.length; i++) {
+            System.out.println((i + 1) + ". " + options[i]);
+        }
+    }
+
+    public void answer() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Choose option number: ");
+        int choice = sc.nextInt();
+
+        if (choice == correct) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Wrong!");
+        }
+    }
+}
